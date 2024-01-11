1476b6e5340e3731c5883b6499d2f5d84be4e4ab
Peter <peta@21cn.com>
2024-01-11 18:12:06 +0800

    new released

---
+++ assets/grid2.658e09a0.js
@@ -0,0 +1,1 @@
+import{t}from"./index.ade6d239.js";const e=t('<div class="grid grid-cols-1 place-content-center place-items-center h-96 w-all text-7xl"><div>Grid2');function l(){return e()}export{l as default};
---
+++ assets/grid2.e4534fb1.js
@@ -0,0 +1,1 @@
+import{t}from"./index.aaf4f82a.js";const e=t('<div class="grid grid-cols-1 place-content-center place-items-center h-96 w-all text-7xl"><div>Grid2');function l(){return e()}export{l as default};
